f459e290a99ebb1d1c61b10adfb60a2faa9f6079
Musee U <icelandisbeingcolouredbywrens@gmail.com>
2012-04-02 14:56:50 -0500

    SO MUCH API (testing code, commented code)

--- lib/zabbixmon.rb
+++ lib/zabbixmon.rb
@@ -1,27 +1,54 @@
 #!/usr/bin/ruby
 
 require 'yaml'
-require './zabbix/api'
+require 'zabbix/api'
 
-config = YAML::load(open('./config.yml'))
+config = YAML::load(open('../config.yml'))
 
 monitor = Zabbix::API.new(config["hostgator"]["url"], config["hostgator"]["user"], config["hostgator"]["password"])
-puts "monitor.api => #{monitor.api}; monitor.debug => #{monitor.debug}"
 
-alert_request = {
-  'method' => 'event.get',
+monitor.login()
+
+apirequest = {
+#  'method' => 'event.get',
+#  'method' => 'alert.get',
+#!  'method' => 'trigger.get',
+#  'method' => 'user.get',
+  'method' => 'usermacro.get',
+#  'method' => 'template.get',
   'params' =>
   {
-    'time_from' => `date --date="1 hour ago" +%s`,
-    'time_till' => `date +%s`,
-    'sortfield' => 'clock',
-    'sortorder' => 'desc',
-#    'limit' => '100',
+#    'time_from' => `date --date="1 hour ago" +%s`,
+#    'time_till' => `date +%s`,
+#    'eventids' => ["41716869", "41716855"],
+#!    'sortfield' => 'lastchange,priority',
+#!    'sortorder' => 'desc',
+#    'only_true' => '1',
+#    'lastChangeSince' => `date --date="30 minutes ago" +%s`,
+#!    'filter' =>
+#!    {
+#      'priority' => ['2', '3', '4', '5'],
+#      'templateid' => '157479'
+#      'status' => '1'
+#!      'value' => ['1'], #0 - OK; 1 - PROBLEM; 2 - UNKNOWN
+#!    },
+#    'expandData' => 'host, hostid',
+#    'select_groups' => 'extend',
+#    'select_items' => 'extend',
+#    'select_functions' => 'extend',
+#    'select_hosts' => 'extend',
+#    'select_dependencies' => 'extend',
+#!    'expandDescription' => '1',
+#    'limit' => '1',
+#!    'min_severity' => '2',
+#    'select_usrgrps' => 'extend',
+#    'select_medias' => 'extend',
+#    'select_mediatypes' => 'extend',
+    'select_templates' => 'extend',
     'output' => 'extend',
-    'value' => '1', #0 - OK; 1 - PROBLEM; 2 - UNKNOWN
   }
 }
 
-last_hundred_alerts = monitor.call_api(alert_request)
+apiresult = monitor.call_api(apirequest)
 
-puts JSON.pretty_generate(last_hundred_alerts)
+puts JSON.pretty_generate(apiresult)
