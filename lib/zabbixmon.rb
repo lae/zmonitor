02446dcf7b2a4731791f1de010db7bc588529cfe
Musee U <icelandisbeingcolouredbywrens@gmail.com>
2012-04-23 05:58:22 -0500

    Initialize an array of hashes with the important pieces of monitored information

--- lib/zabbixmon.rb
+++ lib/zabbixmon.rb
@@ -3,21 +3,33 @@
 require 'yaml'
 require_relative 'zabbix/api'
 
-config = YAML::load(open('profiles.yml'))
-
 profile = "localhost"
+LINES = `tput lines`.to_i
 
+config = YAML::load(open('profiles.yml'))
 monitor = Zabbix::API.new(config[profile]["url"], config[profile]["user"], config[profile]["password"])
-
 current_time = Time.now.to_i
-#triggers = monitor.trigger.get_active(2)
+triggers = monitor.trigger.get_active(2)
 
-event = monitor.event.get_last_by_trigger 13053
+currentevents = []
+
+triggers.each do |t|
+  id = t['triggerid']
+  event = monitor.event.get_last_by_trigger(t['triggerid'])
+  currentevents << {
+    :id => id,
+    :time => t['lastchange'],
+    :severity => t['priority'],
+    :hostname => t['host'],
+    :description => t['description'],
+    :eventid => event['eventid'],
+    :acknowledged => event['acknowledged']
+  }
+end
+
+puts currentevents
+#event = monitor.event.get_last_by_trigger 13053
 #monitor.event.acknowledge([event["eventid"]])
+#monitor.event.acknowledge([monitor.event.get_last_by_trigger()["eventid"]])
 
-#puts JSON.pretty_generate(triggers)
-puts "separate~~"
-puts JSON.pretty_generate(event)
-puts "~~separate"
-
-#puts monitor.whoami
+#puts LINES
