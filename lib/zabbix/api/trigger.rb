3edf9b258c88eb0f52706bb6ef2b45c11359130b
Musee U <icelandisbeingcolouredbywrens@gmail.com>
2012-04-22 23:32:39 -0500

    reduce variable assignment and returns

--- lib/zabbix/api/trigger.rb
+++ lib/zabbix/api/trigger.rb
@@ -12,8 +12,7 @@ module Zabbix
     end
     def get( options = {} )
       request = { 'method' => 'trigger.get', 'params' => options }
-      result = call_api(request)
-      return result
+      return call_api(request)
     end
     def get_active( min_severity = 2 )
       request = {
@@ -31,30 +30,5 @@ module Zabbix
       }
       return call_api(request)
     end
-#    'time_from' => `date --date="1 hour ago" +%s`,
-#    'time_till' => `date +%s`,
-#    'eventids' => ["41716869", "41716855"],
-
-#    'only_true' => '1',
-#    'lastChangeSince' => `date --date="30 minutes ago" +%s`,
-
-#      'host' => ["gator1157", "gator731"]
-#      'priority' => ['2', '3', '4', '5'],
-#      'templateid' => '157479'
-#      'status' => '1'
-#    'select_groups' => 'extend',
-#    'select_items' => 'extend',
-#    'select_functions' => 'extend',
-#    'triggerids' => ['445153'],
-#    'select_hosts' => 'refer',
-#    'select_dependencies' => 'extend',
-#    'select_triggers' => 'extend',
-#    'limit' => '100',
-#    'sendto' => "mullah@openfire.houston.hostgator.com",
-#    'select_usrgrps' => 'extend',
-#    'select_medias' => 'extend',
-#    'select_mediatypes' => 'extend',
-#    'select_templates' => 'extend',
-#    'preservekeys' => '1',
   end
 end
