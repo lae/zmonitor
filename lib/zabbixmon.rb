9ed3cac2daacf761e0ed953b4107f727ce0ced53
Musee U <icelandisbeingcolouredbywrens@gmail.com>
2012-04-23 06:54:54 -0500

    Defined fuzzy time function and added to current events dictionary.

--- lib/zabbixmon.rb
+++ lib/zabbixmon.rb
@@ -6,28 +6,46 @@ require_relative 'zabbix/api'
 profile = "localhost"
 LINES = `tput lines`.to_i
 
+def fuzz(dur)
+  d = dur / 86400
+  h = dur % 86400 / 3600
+  m = dur % 86400 % 3600 / 60
+  s = dur % 86400 % 3600 % 60
+  if d == 0
+    if h == 0
+      if m == 0
+        return "        %2ds" % [ s ]
+      end
+      return "    %2dm %2ds" % [ m, s ]
+    end
+    return "%2dh %2dm %2ds" % [ h, m, s ]
+  end
+  return "%2dd %2dh %2dm" % [ d, h, m ]
+end
+
 config = YAML::load(open('profiles.yml'))
 monitor = Zabbix::API.new(config[profile]["url"], config[profile]["user"], config[profile]["password"])
 current_time = Time.now.to_i
 triggers = monitor.trigger.get_active(2)
 
-currentevents = []
+current_events = []
 
 triggers.each do |t|
-  id = t['triggerid']
   event = monitor.event.get_last_by_trigger(t['triggerid'])
-  currentevents << {
-    :id => id,
-    :time => t['lastchange'],
-    :severity => t['priority'],
+  current_events << {
+    :id => t['triggerid'].to_i,
+    :time => t['lastchange'].to_i,
+    :fuzzytime => fuzz(current_time - t['lastchange'].to_i),
+    :severity => t['priority'].to_i,
     :hostname => t['host'],
     :description => t['description'],
-    :eventid => event['eventid'],
-    :acknowledged => event['acknowledged']
+    :eventid => event['eventid'].to_i,
+    :acknowledged => event['acknowledged'].to_i
   }
 end
 
-puts currentevents
+puts current_time
+puts current_events
 #event = monitor.event.get_last_by_trigger 13053
 #monitor.event.acknowledge([event["eventid"]])
 #monitor.event.acknowledge([monitor.event.get_last_by_trigger()["eventid"]])
